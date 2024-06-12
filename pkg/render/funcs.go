1b02a8aa158e628d7d0421550060be27a02acd0a
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-06-12 13:53:12 +0900

    fix: remove Command function for security

--- pkg/render/funcs.go
+++ pkg/render/funcs.go
@@ -8,7 +8,8 @@ import (
 
 func Funcs(ctx context.Context, tasks map[string]*config.Task, dir string, envs []string) map[string]any {
 	return map[string]any{
-		"Command": NewCommand(ctx, nil, dir, envs).Run,
-		"Task":    NewTask(ctx, tasks, dir, envs).Run,
+		// Remove Command for security
+		// "Command": NewCommand(ctx, nil, dir, envs).Run,
+		"Task": NewTask(ctx, tasks, dir, envs).Run,
 	}
 }
