eed698a2a5a169e6d69b26b3b59c16a79bec0193
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-06-17 08:29:10 +0900

    feat: merge hidden command and main command

--- pkg/command/command.go
+++ pkg/command/command.go
@@ -10,7 +10,7 @@ const waitDelay = 1000 * time.Hour
 
 func SetCancel(cmd *exec.Cmd) {
 	cmd.Cancel = func() error {
-		return cmd.Process.Signal(os.Interrupt) //nolint:wrapcheck
+		return cmd.Process.Signal(os.Interrupt)
 	}
 	cmd.WaitDelay = waitDelay
 }
